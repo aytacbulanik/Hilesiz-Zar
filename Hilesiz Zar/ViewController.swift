ea41e29902f868ed1811043af71e1cd3bbf0d5ae
Aytac Bulanik <aytacbulanik@hotmail.com>
2024-08-31 15:12:30 +0300

    images added in two arrays

--- Hilesiz Zar/ViewController.swift
+++ Hilesiz Zar/ViewController.swift
@@ -15,14 +15,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var buttonOutP1: UIButton!
     @IBOutlet weak var buttonOutP2: UIButton!
     
+    var solImages = [UIImage]()
+    var sagImages = [UIImage]()
+    var solrandom = 0
+    var sagRandom = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         buttonOutP1.layer.cornerRadius = 8
         buttonOutP2.layer.cornerRadius = 8
+        
+        for i in 1...6 {
+            solImages.append(UIImage(named: "sagZar\(i)")!)
+            sagImages.append(UIImage(named: "solZar\(i)")!)
+        }
+        
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
-        
+        zarImageViewOne.image = solImages[3]
+        zarImageViewTwo.image = sagImages[5]
     }
     
 }
