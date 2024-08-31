6d496ed66ed9d07220a3bccf7e55cc568d0af08e
Aytac Bulanik <aytacbulanik@hotmail.com>
2024-08-31 11:11:23 +0300

    components conneted viewcontroller

--- Hilesiz Zar/ViewController.swift
+++ Hilesiz Zar/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var zarImageViewOne: UIImageView!
+    @IBOutlet weak var zarImageViewTwo: UIImageView!
+    @IBOutlet weak var buttonOutP1: UIButton!
+    @IBOutlet weak var buttonOutP2: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+    }
+    
 }
 
